d53d6ef54ed27bd2096efb4da87086487ea7480b
Jared Huffstutler <jaredhuff85@gmail.com>
2023-01-27 13:04:15 -0600

    deploy

--- src/pages/AuthRoutes.jsx
+++ src/pages/AuthRoutes.jsx
@@ -67,7 +67,8 @@ export default function AuthRoutes() {
 
   return (
     <Container fluid>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <h1>WELL THEN...</h1>
     </Container>
   );
 }
