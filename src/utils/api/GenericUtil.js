7491a6a99e553b102ecb96604a660e358a261f83
Jared Huffstutler <jaredhuff85@gmail.com>
2023-02-23 07:59:50 -0600

    deploy

--- src/utils/api/GenericUtil.js
+++ src/utils/api/GenericUtil.js
@@ -5,7 +5,6 @@ export default class GenericUtil {
     // this.baseUrl = `http://localhost:8080/api/${endpoint}`;
     this.baseUrl = ` https://ecomm-server-dev-env.eba-rzt7pshn.us-east-1.elasticbeanstalk.com/api/${endpoint}`;
 
-
     // Bind class methods.
     this.findAll = this.findAll.bind(this);
     this.findOne = this.findOne.bind(this);
